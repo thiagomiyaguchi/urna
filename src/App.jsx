27179419f7b006ce719192f2355edf6c04f5ed8d
thiagomiyaguchi <89756000+thiagomiyaguchi@users.noreply.github.com>
2023-02-16 22:27:46 -0300

    Removido eventListener, adicionado onClick

--- src/App.jsx
+++ src/App.jsx
@@ -5,21 +5,30 @@ function App() {
   const [digito1, setDigito1] = useState(null);
   const [digito2, setDigito2] = useState(null);
 
-  useEffect(() => {
-    const botao = document.querySelectorAll('.numero');
-    botao.forEach((item) => {
-      item.addEventListener('click', mostraDigito);
-    });
-    return () =>
-      botao.forEach((item) => {
-        item.removeEventListener('click', mostraDigito);
-      });
-  }, [digito1]);
+  // useEffect(() => {
+  //   const botao = document.querySelectorAll('.numero');
+  //   botao.forEach((item) => {
+  //     item.addEventListener('click', mostraDigito);
+  //   });
+  //   return () =>
+  //     botao.forEach((item) => {
+  //       item.removeEventListener('click', mostraDigito);
+  //     });
+  // }, [digito1]);
 
   const mostraDigito = (e) => {
-    digito1 ? setDigito2(e.target.innerText) : setDigito1(e.target.innerText);
-    console.log(e.target.innerText);
+    return digito2
+      ? null
+      : digito1
+      ? setDigito2(e.target.innerText)
+      : setDigito1(e.target.innerText);
   };
+
+  const corrige = () => {
+    setDigito1(null);
+    setDigito2(null);
+  };
+
   return (
     <div className='App'>
       <h1>Votação Colégio Trivium</h1>
@@ -30,18 +39,40 @@ function App() {
           <span>{digito2}</span>
         </div>
         <div className='teclas'>
-          <button className='numero'>1</button>
-          <button className='numero'>2</button>
-          <button className='numero'>3</button>
-          <button className='numero'>4</button>
-          <button className='numero'>5</button>
-          <button className='numero'>6</button>
-          <button className='numero'>7</button>
-          <button className='numero'>8</button>
-          <button className='numero'>9</button>
-          <button className='numero zero'>0</button>
-          <button className='branco'>Branco</button>
-          <button>Corrige</button>
+          <button onClick={mostraDigito} className='numero'>
+            1
+          </button>
+          <button onClick={mostraDigito} className='numero'>
+            2
+          </button>
+          <button onClick={mostraDigito} className='numero'>
+            3
+          </button>
+          <button onClick={mostraDigito} className='numero'>
+            4
+          </button>
+          <button onClick={mostraDigito} className='numero'>
+            5
+          </button>
+          <button onClick={mostraDigito} className='numero'>
+            6
+          </button>
+          <button onClick={mostraDigito} className='numero'>
+            7
+          </button>
+          <button onClick={mostraDigito} className='numero'>
+            8
+          </button>
+          <button onClick={mostraDigito} className='numero'>
+            9
+          </button>
+          <button onClick={mostraDigito} className='numero zero'>
+            0
+          </button>
+          <button onClick={mostraDigito} className='branco'>
+            Branco
+          </button>
+          <button onClick={corrige}>Corrige</button>
           <button>Confirma</button>
         </div>
       </div>
